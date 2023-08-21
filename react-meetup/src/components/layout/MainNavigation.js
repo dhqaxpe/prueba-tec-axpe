07b94f211e21a8b4dd694f45dc1d6fc9d14aef81
Daniel Heras Quesada <dherasq@axpe.com>
2023-08-21 19:07:05 +0200

    Ej1 completed

--- react-meetup/src/components/layout/MainNavigation.js
+++ react-meetup/src/components/layout/MainNavigation.js
@@ -1,10 +1,35 @@
+import { useEffect, useState } from "react";
 import { ALL_MEETUP_PAGE, FAVORITES_PAGE, NEW_MEETUP_PAGE } from "./../../utils/constants";
 
 import classes from "./MainNavigation.module.css";
 
 export default function MainNavigation({ setPage }) {
+
+  const [showNavbar, setShownavbar] = useState(true)
+  const [currentScroll, setCurrentScroll] = useState(0)
+
+  const whenScroll = () => {
+    if (window.scrollY > currentScroll) {
+      setShownavbar(false)
+    } else {
+      setShownavbar(true)
+    }
+
+    setCurrentScroll(window.scrollY)
+  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      window.addEventListener('scroll', whenScroll)
+    }
+
+    return () => {
+      window.removeEventListener('scroll', whenScroll)
+    }
+  })
+
   return (
-    <header className={classes.header} data-test="navigation-header">
+    <header className={`${classes.header} ${showNavbar ? classes.showHeader : ''}`} data-test="navigation-header">
       <div className={classes.logo}>React Meetups</div>
       <nav>
         <ul>
